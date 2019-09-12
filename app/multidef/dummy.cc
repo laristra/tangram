d7313de5166ecc900bbf625b34c0e516366633d2
Rao Garimella <rao@lanl.gov>
2019-09-12 12:49:57 -0600

    app to check for multiply defined functions

---
+++ app/multidef/dummy.cc
@@ -0,0 +1,18 @@
+/*
+This file is part of the Ristra Tangram project.
+Please see the license file at the root of this repository, or at:
+    https://github.com/laristra/tangram/blob/master/LICENSE
+*/
+
+// Dummy subroutine which also includes same headers as main.cc to
+// check if we end up with multiply defined symbols because of
+// function definitions in included in header files (class variables
+// are always inlined)
+
+#include <iostream>
+#include "tangram_includes_to_check.h"
+
+int dummy(int i) {
+  std::cout << "Test function to see if we get multiply defined symbols\n";
+  return i;
+}
