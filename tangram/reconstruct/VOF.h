2717195cb3fa21ac1dd28e3552cc82d311393e1e
Eugene Kikinzon <kikinzon@lanl.gov>
2020-03-04 12:25:14 -0700

    Relaxing 3D VOF tests tolerances

--- tangram/reconstruct/VOF.h
+++ tangram/reconstruct/VOF.h
@@ -129,9 +129,8 @@ public:
                           const std::vector< MatPoly<Dim> >& mixed_polys,
                           Plane_t<Dim>& cutting_plane,
                           const bool planar_faces) const {
-#ifdef DEBUG
     double vol_tol = ims_tols_[0].fun_eps;
-#endif
+
     std::vector<int> istencil_cells;
     mesh_.cell_get_node_adj_cells(cellID, Entity_type::ALL, &istencil_cells);
     istencil_cells.insert(istencil_cells.begin(), cellID);
