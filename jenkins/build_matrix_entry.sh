98fdef1c2403200ecaf5b2d19827ceb86cd19368
Rao Garimella <rao@abyzou.lanl.gov>
2020-03-22 16:54:19 -0600

    update to use new wonton and new syntax

--- jenkins/build_matrix_entry.sh
+++ jenkins/build_matrix_entry.sh
@@ -23,14 +23,10 @@ fi
 
 # set modules and install paths
 
-jali_version=1.0.4
 xmof2d_version=0.9.5
-lapack_version=3.8.0
-thrust_version=1.8.3
 
 export NGC=/usr/local/codes/ngc
 ngc_include_dir=$NGC/private/include
-thrust_dir=$NGC/private/thrust/${thrust_version}
 
 # compiler-specific settings
 if [[ $compiler == "intel18" ]]; then
@@ -38,59 +34,47 @@ if [[ $compiler == "intel18" ]]; then
   cxxmodule=intel/${intel_version}
   # openmpi version that libs were built against
   openmpi_version=2.1.2
-  # openmpi module for compiling and linking
-  mpi_module=openmpi/2.1.2
-  jali_install_dir=$NGC/private/jali/${jali_version}-intel-${intel_version}-openmpi-${openmpi_version}
+  mpi_module=openmpi/${openmpi_version}
   xmof2d_install_dir=$NGC/private/xmof2d/${xmof2d_version}-intel-${intel_version}
-  lapacke_dir=$NGC/private/lapack/${lapack_version}-patched-intel-${intel_version}
+  wonton_install_dir=$NGC/private/wonton/new-cmake-intel-${intel_version}
 elif [[ $compiler == "gcc6" ]]; then
   gcc_version=6.4.0
   cxxmodule=gcc/${gcc_version}
   # openmpi version that libs were built against
   openmpi_version=2.1.2
-  # openmpi module for compiling and linking
-  mpi_module=openmpi/2.1.2
-  jali_install_dir=$NGC/private/jali/${jali_version}-gcc-${gcc_version}-openmpi-${openmpi_version}
+  mpi_module=openmpi/${openmpi_version}
   xmof2d_install_dir=$NGC/private/xmof2d/${xmof2d_version}-gcc-${gcc_version}
-  lapacke_dir=$NGC/private/lapack/${lapack_version}-patched-gcc-${gcc_version}
 elif [[ $compiler == "gcc7" ]]; then
   gcc_version=7.3.0
   cxxmodule=gcc/${gcc_version}
   # openmpi version that libs were built against
   openmpi_version=2.1.2
-  # openmpi module for compiling and linking
-  mpi_module=openmpi/2.1.2
-  jali_install_dir=$NGC/private/jali/${jali_version}-gcc-${gcc_version}-openmpi-${openmpi_version}
+  mpi_module=openmpi/${openmpi_version}
   xmof2d_install_dir=$NGC/private/xmof2d/${xmof2d_version}-gcc-${gcc_version}
-  lapacke_dir=$NGC/private/lapack/${lapack_version}-patched-gcc-${gcc_version}
+  wonton_install_dir=$NGC/private/xmof2d/${xmof2d_version}-gcc-${gcc_version}
 elif [[ $compiler == "gcc8" ]]; then
   gcc_version=8.2.0
   cxxmodule=gcc/${gcc_version}
   # openmpi version that libs were built against
   openmpi_version=3.1.3
-  # openmpi module for compiling and linking
-  mpi_module=openmpi/3.1.3
-  jali_install_dir=$NGC/private/jali/${jali_version}-gcc-${gcc_version}-openmpi-${openmpi_version}
+  mpi_module=openmpi/${openmpi_version}
   xmof2d_install_dir=$NGC/private/xmof2d/${xmof2d_version}-gcc-${gcc_version} 
-  lapacke_dir=$NGC/private/lapack/${lapack_version}-patched-gcc-${gcc_version} 
+  wonton_install_dir=$NGC/private/xmof2d/${xmof2d_version}-gcc-${gcc_version}
 fi
 
-jali_flags="-D Jali_DIR:FILEPATH=$jali_install_dir/lib"
-xmof2d_flags="-D XMOF2D_DIR:FILEPATH=$xmof2d_install_dir/share/cmake"
+wonton_flags="-D WONTON_ROOT:FILEPATH=$wonton_install_dir"
+xmof2d_flags="-D ENABLE_XMOF2D=True -D XMOF2D_ROOT:FILEPATH=$xmof2d_install_dir/share/cmake"
 mpi_flags="-D ENABLE_MPI=True"
-lapacke_flags="-D LAPACKE_DIR:FILEPATH=$lapacke_dir"
 
 # build-type-specific settings
 cmake_build_type=Release
-mpi_flags="-D ENABLE_MPI=True"
 thrust_flags=
 if [[ $build_type == "debug" ]]; then
     cmake_build_type=Debug
 elif [[ $build_type == "serial" ]]; then
     mpi_flags=
-    jali_flags=    # jali is not available in serial
 elif [[ $build_type == "thrust" ]]; then
-    thrust_flags="_D ENABLE_THRUST=True -DTHRUST_DIR:FILEPATH=${thrust_dir}"
+    thrust_flags="-D ENABLE_THRUST=True"
 fi
 
 export SHELL=/bin/sh
@@ -99,7 +83,7 @@ export MODULEPATH=""
 . /opt/local/packages/Modules/default/init/sh
 module load $cxxmodule
 module load ${mpi_module}
-module load cmake # 3.0 or higher is required
+module load cmake/3.14.0  # 3.13 or higher is required
 
 echo $WORKSPACE
 cd $WORKSPACE
@@ -108,18 +92,13 @@ mkdir build
 cd build
 
 cmake \
-  -D CMAKE_C_COMPILER=`which mpicc` \
-  -D CMAKE_CXX_COMPILER=`which mpiCC` \
   -D CMAKE_BUILD_TYPE=$cmake_build_type \
   -D ENABLE_UNIT_TESTS=True \
   -D ENABLE_APP_TESTS=True \
   -D ENABLE_JENKINS_OUTPUT=True \
   -D NGC_INCLUDE_DIR:FILEPATH=$ngc_include_dir \
-  -D BOOST_ROOT:FILEPATH=$boost_dir \
   $mpi_flags \
-  $jali_flags \
   $xmof2d_flags \
-  $lapacke_flags \
   $thrust_flags \
   ..
 make -j2
